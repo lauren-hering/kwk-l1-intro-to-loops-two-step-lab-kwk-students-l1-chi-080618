067ddda59794409bc33ee8d36b96379048bbeec1
lauren-hering <lauren.hering@comcast.net>
2018-08-07 21:28:26 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -30,6 +30,16 @@ def first_steps
   end 
 end
     
+def a_few_more_steps
+  loop do
+    puts "Right foot back"
+    sleep(0.5)
+    puts "Left foot back"
+    sleep(0.5)
+    puts "Right foot back"
+    sleep(0.5)
+    puts "Stop"
+    sleep(1)
     
     
     
